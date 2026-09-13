f9009c6edd85be1297fc71bff28bac259b7f0e33
mmdbb71912126-star <mmdbb71912126@gmail.com>
2026-09-13 15:51:41 +0800

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,4 @@
+const SUPABASE_URL = 'https://qziqskgvzabuynduyuqy.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_qrHRKa8lklzuQ53e6ZSs6w_r2tXphJT';
 
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
