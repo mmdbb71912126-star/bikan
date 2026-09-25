cb7514246597c0067fdce9d427b6072f8211d80d
mmdbb71912126-star <mmdbb71912126@gmail.com>
2026-09-25 15:15:47 +0800

    Delete js directory

--- js/supabase.js
+++
@@ -1,4 +0,0 @@
-const SUPABASE_URL = 'https://qziqskgvzabuynduyuqy.supababse.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_qrHRKa8lklzuQ53e6ZSs6w_r2tXphJT';
-
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
